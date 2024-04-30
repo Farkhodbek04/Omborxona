631fc9fda579dd631370f6574d32c23eb27e9aa1
Farxodbek <farhodbekjaloldinov@gmail.com>
2024-04-30 15:24:30 +0500

    DB has been updated.

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'unit', 'quantity', 'price', 'created_at', 'updated_at']
+
+
+@admin.register(ProductInput)
+class ProductInputAdmin(admin.ModelAdmin):
+    list_display = ['product', 'description', 'created_at', 'responsible_user_id']
+
+
+@admin.register(ProductOutput)
+class ProductOutputAdmin(admin.ModelAdmin):
+    list_display = ['product', 'description', 'removed_at', 'responsible_user_id']
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'position', 'tel_number',
+                    'created_at', 'is_staff')  # Specify the fields to display in the admin interface
+
+
+# Register the custom User model and the custom admin class
+admin.site.register(User, CustomUserAdmin)
+
--- product/models.py
+++ product/models.py
@@ -7,6 +7,13 @@ class User(AbstractUser):
     tel_number = models.CharField(max_length=13)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Foydalanuvchi'
+        verbose_name_plural = 'Foydalanuvchilar'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.position}'
+
 
 class CoreModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,6 +22,10 @@ class CoreModel(models.Model):
 
 class Category(CoreModel):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Kategoriya'
+        verbose_name_plural = 'Kategoriyalar'
 
     def __str__(self):
         return self.name
@@ -26,6 +37,7 @@ class Product(CoreModel):
         verbose_name_plural = 'Maxsulotlar'
 
     name = models.CharField(max_length=255)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
     unit = models.CharField(max_length=30)
     quantity = models.DecimalField(decimal_places=2, max_digits=8)
     password = models.CharField(max_length=20)
@@ -47,10 +59,11 @@ class ProductInput(models.Model):
     responsible_user_id = models.ForeignKey(User,
                                             on_delete=models.SET_NULL,
                                             null=True,
-                                            related_name='responsible_user_id')
+                                            related_name='product_input')
 
     def __str__(self):
-        return f'{self.product.name} - Kirim'
+        return f'{self.product.name}({self.product.category_id}) - Kirim'
+
 
 class ProductOutput(models.Model):
     class Meta:
@@ -62,7 +75,7 @@ class ProductOutput(models.Model):
     removed_at = models.DateTimeField(auto_now_add=True)
     responsible_user_id = models.ForeignKey(User,
                                             on_delete=models.SET_NULL, null=True,
-                                            related_name='responsible_user_id')
+                                            related_name='product_output')
 
     def __str__(self):
-        return f'{self.product.name} - Chiqim'
+        return f'{self.product.name}({self.product.category_id}) - Chiqim'
