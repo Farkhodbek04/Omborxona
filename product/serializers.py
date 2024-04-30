d333c6eb36d5533cc5a574a7e37761694d3625cd
Farxodbek <farhodbekjaloldinov@gmail.com>
2024-04-30 16:12:03 +0500

    Serializers has been added!

---
+++ product/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import *
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductInput
+        fields = '__all__'
+
+
+class ProductOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOutput
+        fields = '__all__'
