9e7199ab47b4ebaaaf9bf76f65a49db16fe8ea39
Farxodbek <farhodbekjaloldinov@gmail.com>
2024-05-05 17:23:53 +0500

    Some comments have been updated!

--- product/urls.py
+++ product/urls.py
@@ -10,12 +10,16 @@ router.register(r'units', UnitCRUDview, basename='unit')
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Storekeeper
     path('storekeepers/', SetStorekeeperView.as_view(), name='storekeeper-list'), # get list of all storekeepers
     path('storekeepers/<int:pk>/', SetStorekeeperView.as_view(), name='storekeeper-detail'), # Update, delete, get specific storekeeper with pk
+    # Product
     path('product/', ProductCRUDView.as_view() ), # get all products
     path('product/<int:pk>/', ProductCRUDView.as_view()), # Update, delete, get product with pk
+    # Input
     path('product-input/', ProductInputView.as_view()),
     path('product-input/<int:pk>/', ProductInputView.as_view()),
+    # Output
     path('product-output/', ProductOutputView.as_view()),
     path('product-output/<int:pk>/', ProductOutputView.as_view()),
 ]
