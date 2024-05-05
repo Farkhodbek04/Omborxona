81084b9a2fd9c662909e7a41ffea4353871f4286
Farxodbek <farhodbekjaloldinov@gmail.com>
2024-05-05 11:54:20 +0500

    Product CRUD has been added.

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryCRUDview, UnitCRUDview, SetStorekeeperView
+from .views import CategoryCRUDview, UnitCRUDview, SetStorekeeperView, ProductCRUDView
 
 router = DefaultRouter()
 router.register(r'categories', CategoryCRUDview, basename='category')
@@ -12,5 +12,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('storekeepers/', SetStorekeeperView.as_view(), name='storekeeper-list'),
     path('storekeepers/<int:pk>/', SetStorekeeperView.as_view(), name='storekeeper-detail'),
+    path('product/', ProductCRUDView.as_view() ),
+    path('product/<int:pk>/', ProductCRUDView.as_view())
 
 ]
--- product/views.py
+++ product/views.py
@@ -46,23 +46,64 @@ class SetStorekeeperView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request):
+    def delete(self, request, pk):
             # Check if the user is an accountant
             if request.user.position != 0:  # Assuming 0 represents the position of an accountant
                 return Response({"message": "You do not have permission to perform this action"}, status=status.HTTP_403_FORBIDDEN)
             
-            user_id = request.data.get('user_id')  # Assuming user_id is sent in the request data
-            user = get_object_or_404(User, id=user_id)
+            user = get_object_or_404(User, id=pk)
             user.delete()
             return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
     
     def put(self, request, pk):
         try:
             user = User.objects.get(pk=pk)
-        except User.DoesNotExist:
+            serializer = UserSerializer(user, data=request.data)
+        except user.DoesNotExist:
             return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProductCRUDView(APIView):
+
+    def get(self, request, pk=None):
+        if pk is not None:
+            try:
+                queryset = Product.objects.get(pk=pk)
+                serializer = ProductSerializer(queryset)
+                return Response(serializer.data)
+            except Product.DoesNotExist:
+                return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+            return Response(serializer.data)
+        
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Your product has been created successfuly!"})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product, data=request.data)
+        except Product.DoesNotExist:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            item = Product.objects.get(pk=pk)
+            item.delete()
+            return Response({"message": "The product has been deleted."})
+        except item.DoesNotExist:
+            return Response({"message": "Product does not exist!"})
+        
